7f60dc1c3b63d7dcc634f6ecf432e4209ee4cfbc
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-09-21 14:22:07 -0500

    Delete sales_agent_invoices.sql

--- Chinook/sales_agent_invoices.sql
+++
@@ -1,9 +0,0 @@
---6. Provide a query that shows the invoices associated with each sales agent. The resultant table should include the Sales Agent's full name.
-
-Select Employee.Firstname + ' ' + Employee.LastName as SalesAgentName, Invoice.InvoiceId
-	From Employee
-		Join Customer
-			On Employee.EmployeeId = Customer.SupportRepId
-			 Join Invoice
-				On Customer.CustomerId = Invoice.CustomerId
-					Where Employee.Title Like '%agent'
