f91ec90d8f03a2cba9df71c260619f85c899bfd6
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-09-21 14:31:27 -0500

    Delete invoice_37_line_item_count.sql

--- Chinook/Chinook/invoice_37_line_item_count.sql
+++
@@ -1,5 +0,0 @@
---10. Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
-
-Select Count(*) AS "ID:37 Line Items"
-	FROM InvoiceLine
-		Where InvoiceLine.InvoiceId LIKE '37'
