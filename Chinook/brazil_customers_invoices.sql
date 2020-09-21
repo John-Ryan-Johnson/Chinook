5ca918933f6e44ee414c49909a14a695f5b35d4b
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-09-21 14:21:50 -0500

    Delete brazil_customers_invoices.sql

--- Chinook/brazil_customers_invoices.sql
+++
@@ -1,8 +0,0 @@
-
---3. Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
-SELECT FirstName + ' ' + LastName AS FullName, InvoiceId, InvoiceDate, BillingCountry
-	FROM Customer
-		join Invoice
-			on Customer.CustomerId = Invoice.CustomerId
-				WHERE Invoice.BillingCountry like 'Brazil'
-
