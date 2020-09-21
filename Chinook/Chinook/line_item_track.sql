245c9fff992e80eaa55328e8782aae4ccc8e521d
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-09-21 14:31:44 -0500

    Delete line_item_track.sql

--- Chinook/Chinook/line_item_track.sql
+++
@@ -1,6 +0,0 @@
---12. Provide a query that includes the purchased track name with each invoice line item.
-
-SELECT Track.Name AS "Track Name", InvoiceLine.InvoiceLineId AS "Line Item"
-	FROM Track
-		JOIN InvoiceLine
-			ON InvoiceLine.TrackId = Track.TrackId
