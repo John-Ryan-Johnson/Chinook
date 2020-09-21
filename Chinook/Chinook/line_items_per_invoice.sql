015aa525da8ce6224f2ae471e80406e20a523a1c
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-09-21 14:32:03 -0500

    Delete line_items_per_invoice.sql

--- Chinook/Chinook/line_items_per_invoice.sql
+++
@@ -1,5 +0,0 @@
---11. Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
-
-SELECT InvoiceId, Count(*) as "Line Items"
-	FROM InvoiceLine
-		GROUP BY InvoiceId
