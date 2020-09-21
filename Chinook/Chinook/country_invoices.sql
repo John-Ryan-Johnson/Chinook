8c07059ddb4aaec9ac830eff143857d3ca39d90f
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-09-21 14:31:20 -0500

    Delete country_invoices.sql

--- Chinook/Chinook/country_invoices.sql
+++
@@ -1,5 +0,0 @@
---14. Provide a query that shows the # of invoices per country. HINT: GROUP BY
-
-SELECT Invoice.BillingCountry, Count(*) As "Number Of Invoices"
-	FROM Invoice
-		GROUP BY Invoice.BillingCountry
