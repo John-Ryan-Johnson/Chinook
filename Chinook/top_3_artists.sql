ce29c6099817fe946644fc3c816ce76221609736
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-09-28 18:30:46 -0500

    Update top_3_artists.sql

--- Chinook/top_3_artists.sql
+++ Chinook/top_3_artists.sql
@@ -1,14 +1,12 @@
 --26. Provide a query that shows the top 3 best selling artists.
 
 SELECT TOP(3) Artist.Name, COUNT(*) AS [Tracks Sold]
-	FROM InvoiceLine
+FROM InvoiceLine
 		JOIN Track
 			ON InvoiceLine.TrackId = Track.TrackId
-				JOIN Album
-					ON Album.AlbumId = Track.AlbumId
-						JOIN Artist
-							ON Album.ArtistId = Artist.ArtistId
-								JOIN Invoice
-									ON InvoiceLine.InvoiceId = Invoice.InvoiceId
-										GROUP BY Artist.Name
-											ORDER BY [Tracks Sold] DESC
+		JOIN Album
+			ON Album.AlbumId = Track.AlbumId
+		JOIN Artist
+			ON Album.ArtistId = Artist.ArtistId
+GROUP BY Artist.Name
+ORDER BY [Tracks Sold] DESC
